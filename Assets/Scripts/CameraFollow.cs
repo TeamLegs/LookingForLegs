25d01fecb0972b6c802da918aa5361e4327f6812
George Fyles <2100707@uad.ac.uk>
2022-10-30 15:18:33 +0000

    Added camera functionality

--- Assets/Scripts/CameraFollow.cs
+++ Assets/Scripts/CameraFollow.cs
@@ -7,19 +7,27 @@ public class CameraFollow : MonoBehaviour
     //Declare variables
     [SerializeField] private Transform target;
     [SerializeField] private Vector3 offset;
+    [SerializeField] private float xMin;
+    [SerializeField] private float xMax;
     [SerializeField] private Transform backgroundImg;
 
     // Start is called before the first frame update
     void Start()
     {
-        
     }
 
     // Update is called once per frame
     void Update()
     {
         //Sets the current position of camera to the target and offsets its position.
-        var pos = target.position + offset;
-        transform.position = pos;
+        transform.position = target.position + offset;
+        if(transform.position.x > xMax)
+        {
+            transform.position = new Vector3(xMax, transform.position.y, transform.position.z);
+        }
+        else if(transform.position.x < xMin)
+        {
+            transform.position = new Vector3(xMin, transform.position.y, transform.position.z);
+        }
     }
 }
