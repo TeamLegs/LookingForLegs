3ab4112d91c066ae44f296661f935a732bccf60a
Sam Collier <samcollier916@gmail.com>
2022-10-30 14:36:15 +0000

    Parallax Scene

--- Assets/Scripts/ParallaxEffect.cs
+++ Assets/Scripts/ParallaxEffect.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Serialization;
 
 public class ParallaxEffect : MonoBehaviour
 {
@@ -8,13 +9,15 @@ public class ParallaxEffect : MonoBehaviour
     [SerializeField] private float parallaxEffect;
     [SerializeField] private Camera cam;
     [SerializeField] private SpriteRenderer sprite;
+    [SerializeField] private bool lockInitialY = true;
 
-    private float startX, length;
+    private float startX, length, startY;
     
     // Start is called before the first frame update
     void Start()
     {
         startX = transform.position.x;
+        startY = transform.position.y;
         length = sprite.bounds.size.x * transform.childCount;
     }
 
@@ -26,7 +29,15 @@ public class ParallaxEffect : MonoBehaviour
         float dist = camX * parallaxEffect;
 
         var pos = transform.position;
-        transform.position = new Vector3(startX + dist, pos.y, pos.z);
+        
+        if (lockInitialY)
+        {
+            transform.position = new Vector3(startX + dist, startY, pos.z);
+        }
+        else
+        {
+            transform.position = new Vector3(startX + dist, pos.y, pos.z);
+        }
 
         if (temp > startX + length/2)
         {
